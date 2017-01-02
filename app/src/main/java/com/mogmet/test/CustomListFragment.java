5f6b29c564abbd9c64fd1ed3f10b23d97320d295
mogmet <mogmet@gmail.com>
2017-01-02 22:06:20 +0900

    [add] RecyclerViewの完成

--- app/src/main/java/com/mogmet/test/CustomListFragment.java
+++ app/src/main/java/com/mogmet/test/CustomListFragment.java
@@ -2,10 +2,17 @@ package com.mogmet.test;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+import butterknife.BindView;
 import butterknife.ButterKnife;
 
 /**
@@ -13,6 +20,10 @@ import butterknife.ButterKnife;
  */
 
 public class CustomListFragment extends Fragment {
+    @BindView(R.id.custom_recycler_view)
+    RecyclerView customRecyclerView;
+    ArrayList<Map<String, String>> mData;
+
     public static CustomListFragment newInstance() {
         return new CustomListFragment();
     }
@@ -22,6 +33,14 @@ public class CustomListFragment extends Fragment {
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_custom_list, container, false);
         ButterKnife.bind(this, view);
+        customRecyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        mData = new ArrayList<Map<String, String>>();
+        HashMap<String, String> map = new HashMap<String, String>();
+        map.put("text", "texttttttttttttt");
+        map.put("buttonText", "button dao");
+        mData.add(map);
+        CustomRecyclerAdapter adapter = new CustomRecyclerAdapter(getContext(), mData);
+        customRecyclerView.setAdapter(adapter);
         return view;
     }
 }
