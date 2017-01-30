17008cec680304644544fd95632eba6c1f5d4d16
mogmet <mogmet@gmail.com>
2017-01-30 23:16:50 +0900

    [add] セルを右端に寄せるように修正

--- app/src/main/java/com/mogmet/test/CustomListFragment.java
+++ app/src/main/java/com/mogmet/test/CustomListFragment.java
@@ -33,7 +33,6 @@ public class CustomListFragment extends Fragment {
         return new CustomListFragment();
     }
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
